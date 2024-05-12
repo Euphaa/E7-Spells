b5f548686d41804ef3ab23480c63d66cddc70eab
Euphaa <135658782+Euphaa@users.noreply.github.com>
2024-05-12 13:26:55 -0400

    Initial commit

---
+++ src/client/java/com/e7/spells/E7SpellsClient.java
@@ -0,0 +1,10 @@
+package com.e7.spells;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class E7SpellsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
